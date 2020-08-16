9667f7bdddbb21d9660334e33b29597d676094b3
Euclidean13 <aacristellys@icloud.com>
2020-08-16 20:39:40 +0200

    master: Infinite scroll, with loading and futures

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/card_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/input_page.dart';
+import 'package:flutter_components/src/pages/listview_page.dart';
 import 'package:flutter_components/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -15,6 +16,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card'             : (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs'           : (BuildContext context) => InputPage(),
-    'slider'           : (BuildContext context) => SliderPage()
+    'slider'           : (BuildContext context) => SliderPage(),
+    'list'             : (BuildContext context) => ListaPage()
   };
 }
